1b55da0da32d5630a179f542bc0ad3d4eed0aa39
1bnr <9zabner@gmail.com>
2017-03-14 11:52:55 -0500

    changes

--- chat_client.rb
+++ chat_client.rb
@@ -2,39 +2,45 @@
 require "socket"
 require "json"
 class Client
-  @server = server
+  @server = nil
   @request = nil
   @response = nil
   # initialize the connection; failure exits
   def initialize( port_num, ip_address )
-    server = TCPSocket.open( ip_address, port_num )
+    @server = TCPSocket.open( ip_address, port_num )
     puts "Client is not connected."
+    @connected = false
+# TODO check if server connected
+    listen
+    send
+    @request.join
+    @response.join
+  end
+  # prompt user to login or register
+  def register_or_login
     request = [nil, nil, nil]
-    loop do
+    loop {
       puts "to register a new account:\n\tREGISTER <client_ID> <password>\nto login:\n\tLOGIN <client_ID> <password>\n"
       msg = $stdin.gets.chomp.split
       if msg.length == 3 && (msg[0] == 'REGISTER' || msg[0] == 'LOGIN')
         request = [msg[0], msg[1], msg[2] ].to_json
         break
       end
-    end
-    @server.puts(request)
+    }
+    request
+  end # end register_or_login
 
-    listen
-    send
-    @request.join
-    @response.join
-  end
-
+# listen for messages from server
   def listen
     @response = Thread.new do
       loop {
         msg = JSON.parse(@server.gets.chomp)
+        puts sprintf("msg in listen: %s\n", msg.inpect)
         if msg[0].string?
           puts msg[0]
         elsif msg[0] == 0x00
           if msg[1] == "CLIST"
-            puts msg[1].to_s.gsub! /["\[\]]/,''
+            puts msg[1].to_s.gsub!(/["\[\]]/,'')
           elsif msg[1] == "DISCONNECT"
             puts "disconnecting"
             exit(0)
@@ -48,25 +54,37 @@ class Client
         end
       }
     end
-  end
+  end # end listen
 
+  # send messages to char server
   def send
     @request = Thread.new do
+      request = nil
+      msg = nil
       loop {
-        msg = $stdin.gets.chomp.split
-        command = msg.first
-        if command == 'MSG'
-          msg = msg.slice(1, msg.length-1).join(' ')
+        if !@connected
+          request = register_or_login
+          puts sprintf("msg in send: %s\n", msg.inspect)
+          @connected = true
+        else
+          msg = $stdin.gets.chomp.split
+          command = msg.first
+          if command == 'MSG'
+            puts msg.inspect
+            msg = msg.slice(1, msg.length-1).join(' ')
+          else
+            msg = nil
+          end
+          request = [command, msg].to_json
         end
-        request = [command, msg].to_json
         @server.puts( request )
       }
     end
-  end
-end
+  end #end send
+end # end Client
 
 
-
+# launch client with supplied arguements or print usage message
 if ARGV.length != 2 then
   puts "Usage: chat_server <port_num> <ip_address>\n"
 else
--- chat_server.rb
+++ chat_server.rb
@@ -9,85 +9,98 @@ class ChatServer
     @clients = Hash.new
     printf("Chat Server started on port %d\n", port)
     run
-  end
+  end # end initialize
 
   def run
     loop {
       Thread.start(@serverSocket.accept) do | client |
         net_data = JSON.parse(client.gets)
         # exit if login/registration doesn't have 3 args
-
-        if net_data.length != 3 then
-          client.puts([0xFF].to_json)
-          Thread.kill self
-        end
+        puts sprintf("net_data: %s\n", net_data.inspect)
+#        if net_data.length != 3 then
+#          client.puts([0xFF].to_json)
+#          Thread.kill self
+#        end
         command = net_data[0].chomp
-        username = net_data[1].chomp
+        client_ID = net_data[1].chomp
         password = net_data[2].chomp
-        usercheck = check_username(username)
-        # if user is registering, search user.csv for a username match
-        if command == 'REGISTER' then
-          if (usercheck[0]) then # username in use, reject registration
+        usercheck = check_client_ID(client_ID)
+        # if user is registering, search user.csv for a client_ID match
+        if command == 'REGISTER'
+          if (usercheck[0]) then # client_ID in use, reject registration
             client.puts([0x02].to_json)
             Thread.kill self
-          else # username is unique, save the new entry, log user in
-            new_entry = sprintf("%s,%s\n", username, password)
+          else # client_ID is unique, save the new entry, log user in
+            new_entry = sprintf("%s,%s\n", client_ID, password)
             File.open('users.csv', "a+").write(new_entry)
-            @clients[username] = client
+            @clients[client_ID] = client
           end
-        elsif command == 'LOGIN' then
-          if !usercheck[0] || password != usercheck[1].chomp
+        elsif command == 'LOGIN' # user sending LOGIN command
+          # client_ID is registered, and password is correct
+          if usercheck[0] && password == usercheck[1].chomp
+            puts sprintf("user '%s' logged in\n", client_ID)
+            @clients[client_ID] = client
+          else # else send error code, and kill thread
+            puts "login error. kill thread"
             client.puts([0x01].to_json)
             Thread.kill self
-          else
-            @clients[username] = client
           end
         end
-        @clients[username] = client
+        # passed through register/login
+        puts sprintf("'%s' connected\n", client_ID)
+        @clients[client_ID] = client
         client.puts [0x00].to_json
-        listen_user_messages( username, client )
+        listen_user_messages( client_ID, client )
       end
     }.join
-  end
+  end # end run
 
-  def check_username( username )
+  # check if client_ID is in users.csv, returns tuple; if client_ID is found
+  # [true, password], if not found returns [false, nil]
+  def check_client_ID( client_ID )
     if File.file? './users.csv'
       File.open('./users.csv').each_line do | line |
         reg_user = line.split(",")
-        if reg_user[0] == username
+        if reg_user[0] == client_ID
           return [true, reg_user[1]]
         end
       end
     end
     return [false]
-  end
+  end # end check_client_ID
 
-  def listen_user_messages( username, client )
+# listen on client connnection for input messages
+  def listen_user_messages( client_ID, client )
+    puts "in listen_user_messages"
     loop {
       msg = JSON.parse(client.gets.chomp)
+      puts sprintf("msg.inpect : %s\n", msg.inspect)
       command = msg[0]
-      if command == 'MSG' && msg.length <= 2
+      if command == 'MSG'
         @clients.each do |other_name, other_client|
-          unless other_name == username
-            other_client.puts ["#{username.to_s}: #{msg[1]}"].to_json
+          if other_name == client_ID
+            client.puts [0x00, "Success"].to_json
+          else
+            other_client.puts ["#{client_ID.to_s}: #{msg[1]}"].to_json
           end
         end
-        client.puts [0x00, "Success"].to_json
-
       elsif command == 'DISCONNECT'
+        puts sprintf("disconnecting user '%s'", client_ID)
         client.puts [0x00, "DISCONNECT"].to_json
         @clients.remove client
       elsif command == 'CLIST'
         client.puts [0x00, "CLIST", @clients.keys].to_json
       else # catchall for wrong format
+        puts "unknown message format"
         client.puts [0xFF].to_json
       end
-
     }
-  end
+    puts "outside listen loop"
+  end # end listen_user_messages
 
-end
+end # end ChatServer
 
+# launch server with supplied arguements, or display usage message
 if ARGV.length != 2 then
   puts "Usage: chat_server <port_num> <ip_address>\n"
 else
